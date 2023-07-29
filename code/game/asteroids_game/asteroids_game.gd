242dbbe900ed5c1c8bb3f22159a3f2f587519b98
wovri <adammisiak2@gmail.com>
2023-07-29 14:54:54 +0200

    AsteroidsGame complete ✨  - player is able to move  - space_objects spawn randomly

--- code/game/asteroids_game/asteroids_game.gd
+++ code/game/asteroids_game/asteroids_game.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
 @export var space_object_tscn: PackedScene
+var spawn_point_width = 200
 @onready var nonliving_spawn_point = $NonlivingSpawn
 
-func _ready():
-	spawn_space_object()
+
 
 func spawn_space_object():
 	var space_object_inst = space_object_tscn.instantiate()
-	space_object_inst.set_position(nonliving_spawn_point.position)
-	space_object_inst.set_linear_velocity(Vector2(-10, 0))
+	var random_y_spawn_point = randi_range(-spawn_point_width, spawn_point_width)
+	var _current_spawn_point = nonliving_spawn_point.position + Vector2(0, random_y_spawn_point)
+	space_object_inst.set_position(_current_spawn_point)
 	add_child(space_object_inst)
+
+
+func _on_spawn_timer_timeout():
+	spawn_space_object()
--- code/game/celestials/nonliving/space_object.gd
+++ code/game/celestials/nonliving/space_object.gd
@@ -1,9 +1,35 @@
 extends RigidBody2D
 
+var x_max = 20
+var speed = 100
+@onready var y_max = get_viewport_rect().size.y
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	self.queue_free()
-#Extra knockback
+
+
+func _ready():
+	set_up_initial_movement()
+
+
+func _on_body_entered(body):
+	if body is CharacterBody2D:
+		print("Knock him OFF!")
+
+
+func set_up_initial_movement():
+	var rand_x = randi_range(0, x_max)
+	var rand_y = randi_range(0, y_max)
+	var destination_point = Vector2(rand_x, rand_y)
+	var radians_to_destination = self.position.angle_to_point(destination_point)
+	self.set_linear_velocity(Vector2(speed, 0).rotated(radians_to_destination))
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	$NotVisibleLifeTime.queue_free()
+
 
 func _on_life_time_timeout():
+	self.queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	self.queue_free()

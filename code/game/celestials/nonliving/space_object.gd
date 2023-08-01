3b4061c751cb7c91f12ead6104898ecf28b79b4a
wovri <adammisiak2@gmail.com>
2023-08-01 06:43:44 +0200

    Make player 'space' movement and add knockback ✨

--- code/game/celestials/nonliving/space_object.gd
+++ code/game/celestials/nonliving/space_object.gd
@@ -2,17 +2,28 @@ extends RigidBody2D
 
 var x_max = 20
 var speed = 100
+@export var force_factor = 100
+@export var scale_min = 0.5
+@export var scale_max = 3
 @onready var y_max = get_viewport_rect().size.y
+@onready var scalables = [$Pivot, $CollisionShape2D]
 
 
 
 func _ready():
+	change_size()
 	set_up_initial_movement()
 
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		print("Knock him OFF!")
+		body.knockback(mass * force_factor, global_position)
+
+func change_size():
+	var scale_factor = randf_range(scale_min, scale_max)
+	mass = scale_factor
+	for scalable in scalables:
+		scalable.scale = Vector2(scale_factor, scale_factor)
 
 
 func set_up_initial_movement():
